ab38628f5a4ef54ac0c48027761c377a9f0b89a1
Obedsama <ohwo.okoro@yahoo.com>
2022-07-11 02:43:35 +0100

    created a function that randomly selects from three words

---
+++ script.js
@@ -0,0 +1,8 @@
+function computerPlay() {
+    // Make a variable to hold for rock, papper and scissor
+    let gameWords = ['Rock', 'Paper', 'Scissors'];
+    // Randomly selects from the options of words
+    let words = gameWords[Math.floor(Math.random() * gameWords.length)];
+    console.log(words);
+}
+computerPlay();
