e5354b2b30499d26693aa98fbed6744d563c7068
Obedsama <ohwo.okoro@yahoo.com>
2022-07-22 17:47:46 +0100

    created the final winner function and added it to the game function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 function computerPlay() {
     // Make a variable to hold for rock, papper and scissor
     let gameWords = ['Rock', 'Paper', 'Scissors'];
@@ -5,7 +7,6 @@ function computerPlay() {
     let words = gameWords[Math.floor(Math.random() * gameWords.length)];
     return words.toUpperCase();
 }
-// console.log(computerPlay());
 
 function playRound() {
     // Variable for player's selection
@@ -20,31 +21,51 @@ function playRound() {
         // Checks if player's selection is rock or scissor
         if(playerSelection === 'ROCK') {
         // Congratulates player if rock
+            playerScore+= 1;
             return 'Player Wins! Rock beats Scissors'
         } else {
         // Congratulates computer if rock
+            computerScore+= 1;
             return 'You Lose! Rock beats Scissors'
         }
+    // Checks player selection and computer selection for paper or rock
     } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK' || computerSelection === 'PAPER' && playerSelection === 'ROCK') {
+        // Checks if player's selection is paper
         if(playerSelection === 'PAPER') {
+        // Congratulates player if paper
+            playerScore++;
             return 'Player Wins! Paper beats Rock'
         } else {
+        // Congratulates  computer if paper
+            computerScore++;
             return 'You Lose! Paper beats Rock'
         }
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER' || computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
         if(playerSelection === 'SCISSORS') {
+            playerScore++;
             return 'Player Wins! Scissors beats Paper'
         } else {
+            computerScore++;
             return 'You Lose! Scissors beats Paper'
         }
     }
-}
-// console.log(playRound())
+};
+
+function finalWinner() {
+    if(playerScore > computerScore) {
+        return 'HURRAY!!! PLAYER IS THE WINNER'
+    } else if(playerScore < computerScore) {
+        return 'HURRAY!!! COMPUTER IS THE WINNER'
+    } else {
+        return 'OOPS! GAME ENDED IN A DRAW'
+    }
+};
 
 function game() {
     for(let i = 1; i <= 5; i++) {
        console.log(playRound());
     }
-}
+    console.log(finalWinner());
+};
 
 game()
